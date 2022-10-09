e977f0b1d009482028cf34d7246502d554e92245
dshayderov <dshayderov@list.ru>
2022-10-09 21:36:39 +0300

    add zadanie_2

---
+++ Project/zadanie_2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+
+
+def cylinder():
+    way = int(input("Какую площадь вы хотите вычислить?\n"
+                    "Боковой поверхности(1)\n"
+                    "Полную площадь(2)\n"
+                    "> "))
+
+    r = float(input("Введите радиус: "))
+    h = float(input("Введите высоту: "))
+    s = 2 * math.pi * r * h
+
+    if way == 1:
+        print(f"Боковая площадь s = {s}")
+
+    elif way == 2:
+        s += circle(r) * 2
+        print(f"Полная площадь s = {s}")
+
+    else:
+        print("Ошибка")
+
+
+def circle(r):
+    return math.pi * pow(r, 2)
+
+
+if __name__ == '__main__':
+    cylinder()
